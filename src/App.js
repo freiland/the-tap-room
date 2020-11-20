5712654b615f63ea495bba8c2e477a325f154272
Forrest Reiland <you@example.com>
2020-11-20 10:04:45 -0800

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 function App() {
   return (
     <React.Fragment>
+      <div class='jumbotron'>
       <Header />
+      </div>
     </React.Fragment>
   );
 }
