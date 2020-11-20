1b1e3fd1baf34da7182ae03d40316e7b3f3c8541
Forrest Reiland <you@example.com>
2020-11-20 10:42:47 -0800

    add keglist component

---
+++ src/KegList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { propTypes } from 'react-bootstrap/esm/Image';
+import Keg from './Keg';
+
+const masterKegList = [
+  {name: IPA}
+]
+
+function KegList(props) {
+
+  // function handlePouringKegSubmission(event) {
+  //   event.preventDefault();
+  //   console.log(event.target.quantity.value);
+  // }
+
+  return (
+    <React.Fragment>
+      {props.masterKegList.map((keg) =>
+      <Keg name = {keg.name}/>
+      
+      )}
+    </React.Fragment>
+  )
+}
+
+KegList.propTypes = {
+  kegList: propTypes.array, 
+  onKegSelection: propTypes.func,
+  onPourKeg: propTypes.func, 
+  onStockKeg: propTypes.func
+
+}
+
+export default KegList; 
