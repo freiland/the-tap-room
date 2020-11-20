3f2ffba6f32397deb978d0d3e36566340785dc12
Forrest Reiland <you@example.com>
2020-11-20 11:10:42 -0800

    add new keg form

---
+++ src/NewKegForm.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { v4 } from 'uuid';
+import PropTypes from 'prop-types';
+
+function NewKegForm(props) {
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({name: event.target.value, pintsAvail: event.target.value, brand: event.target.value, price: event.target.value, alcoholContent: event.target.value})
+  }
+
+  return (
+    <React.Fragment>
+      <form onSubmit={handleNewKegFormSubmission}>
+      <input 
+      type='text'
+      name='name'
+      placeholder = 'name' />
+    <input
+      type='text'
+      name='pintsAvail'
+      placeholder = "124" />
+      <input
+      type='text'
+      name='brand' 
+      placeholder = 'brand'/>
+    <input
+      name='price'
+      placeholder='price' />
+      <input
+      name='alcoholContent'
+      placeholder='5%' />
+    <button type='submit'>Add Keg</button></form>
+    </React.Fragment>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm;
